b770864dfe9a0d1f8e7bc71b4fe82fc45a2903c9
Aviscad <anthonyviscad@gmail.com>
2022-08-23 21:17:13 -0600

    Rainbow Color Effect and General Changes

--- js/script.js
+++ js/script.js
@@ -6,25 +6,17 @@ let grid = document.querySelector(".grid"),
   normal = document.querySelector("#normal"),
   rainbow = document.querySelector("#rainbow"),
   eraser = document.querySelector("#eraser"),
+  colorChanger,
+  mouseDown,
   clear = document.querySelector("#clear");
 
 window.addEventListener("load", () => {
-  // drawGrid(size);
   createGrid();
   drawInGrid(1);
 });
 
-function getRandomColor() {
-  let r = rndNum(0, 255),
-    g = rndNum(0, 255),
-    b = rndNum(0, 255),
-    rgb = `rgb(${r}, ${g}, ${b})`;
-  return rgb;
-}
-
 function createGrid() {
   showSize.textContent = `${gridValue.value} x ${gridValue.value}`;
-
   grid.innerHTML = "";
   //CREATING DIVS
   for (let i = 1; i <= size; i++) {
@@ -34,7 +26,7 @@ function createGrid() {
       grid.appendChild(div);
     }
   }
-  console.log(size);
+
   //CHANGING GRID SIZE
   grid.style.gridTemplateColumns = "repeat(" + size + ", 1fr)";
   grid.style.gridTemplateRows = "repeat(" + size + ", 1fr)";
@@ -46,16 +38,21 @@ function drawInGrid(option) {
     1. NORMAL
     2. RAINBOW
     3. ERASER
-    4. CLEAR
   */
   if (option == 1) {
     //ADDING CLICK EVENT TO ALL DIVS
     let boxesArray = document.querySelectorAll(".box");
     for (let i = 0; i < boxesArray.length; i++) {
       let currentBox = boxesArray[i];
-      currentBox.addEventListener("click", (e) => {
+      currentBox.addEventListener("mousedown", (e) => {
         let div = e.target;
         div.style.backgroundColor = colorPicker.value;
+      });
+      currentBox.addEventListener("mouseover", (e) => {
+        if (mouseDown) {
+          let div = e.target;
+          div.style.backgroundColor = colorPicker.value;
+        }
       });
     }
   } else if (option == 2) {
@@ -63,9 +60,15 @@ function drawInGrid(option) {
     let boxesArray = document.querySelectorAll(".box");
     for (let i = 0; i < boxesArray.length; i++) {
       let currentBox = boxesArray[i];
-      currentBox.addEventListener("click", (e) => {
+      currentBox.addEventListener("mousedown", (e) => {
         let div = e.target;
         div.style.backgroundColor = getRandomColor();
+      });
+      currentBox.addEventListener("mouseover", (e) => {
+        if (mouseDown) {
+          let div = e.target;
+          div.style.backgroundColor = getRandomColor();
+        }
       });
     }
   } else if (option == 3) {
@@ -73,9 +76,15 @@ function drawInGrid(option) {
     let boxesArray = document.querySelectorAll(".box");
     for (let i = 0; i < boxesArray.length; i++) {
       let currentBox = boxesArray[i];
-      currentBox.addEventListener("click", (e) => {
+      currentBox.addEventListener("mousedown", (e) => {
         let div = e.target;
         div.style.backgroundColor = "#fff";
+      });
+      currentBox.addEventListener("mouseover", (e) => {
+        if (mouseDown) {
+          let div = e.target;
+          div.style.backgroundColor = "#fff";
+        }
       });
     }
   }
@@ -84,54 +93,81 @@ function drawInGrid(option) {
 //CONVERT RGB TO HEX
 function componentToHex(c) {
   let hex = c.toString(16);
-  console.log(hex);
   return hex.length == 1 ? "0" + hex : hex;
 }
 
 function rgbToHex(r, g, b) {
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  return (
+    "#" +
+    componentToHex(parseInt(r)) +
+    componentToHex(parseInt(g)) +
+    componentToHex(parseInt(b))
+  );
 }
 
-rndNum = (min, max) =>
+const rndNum = (min, max) =>
   Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) +
   Math.ceil(min);
 
-normal.addEventListener("click", () => {
+function getRandomColor() {
+  let r = rndNum(0, 255),
+    g = rndNum(0, 255),
+    b = rndNum(0, 255),
+    rgb = `rgb(${r}, ${g}, ${b})`;
+  return rgb;
+}
+
+grid.addEventListener("mousedown", (e) => {
+  e.preventDefault();
+  mouseDown = true;
+});
+
+grid.addEventListener("mouseup", (e) => {
+  e.preventDefault();
+  mouseDown = false;
+});
+
+colorPicker.addEventListener("change", () => {
+  clearInterval(colorChanger);
   drawInGrid(1);
 });
-colorPicker.addEventListener("change", () => {
+
+normal.addEventListener("click", () => {
+  clearInterval(colorChanger);
   drawInGrid(1);
 });
 
 rainbow.addEventListener("click", () => {
   drawInGrid(2);
-  // setInterval(() => {
-  //   let currentRGBCOlor = getRandomColor();
-  //   console.log(currentRGBCOlor);
-  //   let rgbArrayValues = currentRGBCOlor
-  //     .substring(4, currentRGBCOlor.length - 1)
-  //     .split(",");
-
-  //   console.log(
-  //     rgbToHex(rgbArrayValues[0], rgbArrayValues[1], rgbArrayValues[2])
-  //   );
-  // }, 1000);
+  colorChanger = setInterval(() => {
+    let currentRGBCOlor = getRandomColor();
+    let rgbArrayValues = currentRGBCOlor
+      .substring(4, currentRGBCOlor.length - 1)
+      .split(",");
+    colorPicker.value = rgbToHex(
+      rgbArrayValues[0],
+      rgbArrayValues[1],
+      rgbArrayValues[2]
+    );
+  }, 500);
 });
 
 eraser.addEventListener("click", () => {
+  clearInterval(colorChanger);
   colorPicker.value = "#ffffff";
   drawInGrid(3);
 });
 
 clear.addEventListener("click", () => {
+  clearInterval(colorChanger);
   createGrid(size);
+  colorPicker.value = "#000000";
   drawInGrid(1);
 });
 
-gridValue.addEventListener("change", () => {
+gridValue.addEventListener("input", () => {
+  clearInterval(colorChanger);
   size = gridValue.value;
-  console.log(size);
   createGrid();
   drawInGrid(1);
-  // drawGrid(gridValue.value);
 });
